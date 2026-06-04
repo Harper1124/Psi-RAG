7b484efcaab86f8643a713ed259623a36810a533
harper <564127736@qq.com>
2026-06-04 16:47:47 +0800

    pdf拆分结果

--- conf/multimodal_pdf.py
+++ conf/multimodal_pdf.py
@@ -1,6 +1,8 @@
 conf = {
     "read_local_pdf": "file",
-    "embed_name": "qwen3-vl:Qwen/Qwen3-VL-Embedding",
+    "embed_name": "qwen3-vl:Qwen/Qwen3-VL-Embedding-2B",
+    "abs_name": "ollama:qwen2.5:7b",
+    "qa_name": "ollama:qwen2.5:7b",
     "embed_model_kwargs": {
         "batch_size": 4,
         "normalize": True,
