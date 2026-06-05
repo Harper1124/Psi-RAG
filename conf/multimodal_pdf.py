00d23bfd8f23420aa608c27de8c2a3fb4e515a05
harper <564127736@qq.com>
2026-06-05 16:13:09 +0800

    换qwen3-vl

--- conf/multimodal_pdf.py
+++ conf/multimodal_pdf.py
@@ -1,8 +1,8 @@
 conf = {
     "read_local_pdf": "file",
     "embed_name": "qwen3-vl:Qwen/Qwen3-VL-Embedding-2B",
-    "abs_name": "ollama:qwen2.5:7b",
-    "qa_name": "ollama:qwen2.5:7b",
+    "abs_name": "ollama:qwen3-vl:8b",
+    "qa_name": "ollama:qwen3-vl:8b",
     "embed_model_kwargs": {
         "batch_size": 4,
         "normalize": True,
@@ -13,4 +13,6 @@ conf = {
     "hybrid_search": False,
     "rerank": False,
     "tree_build_diagnostics": True,
+    "data_dir": "/data/yzy/psiRAG/Psi-RAG/test.pdf",
+    "read_local_pdf": "file",
 }
